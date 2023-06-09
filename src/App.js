de9b8d0edba2682d32ecf753721f8fc08be87d6d
Chris <christiankurniadi@gmail.com>
2023-06-09 12:47:54 +0700

    change name

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
       <div style={styles.main}>
         <Header
           title="Photo Gallery"
-          subtitle="A simple photo gallery app built by stackup-username"
+          subtitle="A simple photo gallery app built by chris"
         />
         <div style={styles.container}>
           {data.map((image_data, index) => {
